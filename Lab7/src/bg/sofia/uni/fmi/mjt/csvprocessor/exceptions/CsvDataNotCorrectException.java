969766becc7e26e73916e14f44b210d07cf47684
Bayryam <116573641+Bayryam@users.noreply.github.com>
2023-12-05 00:40:32 +0200

    Upload Java Lab7

---
+++ Lab7/src/bg/sofia/uni/fmi/mjt/csvprocessor/exceptions/CsvDataNotCorrectException.java
@@ -0,0 +1,11 @@
+package bg.sofia.uni.fmi.mjt.csvprocessor.exceptions;
+
+public class CsvDataNotCorrectException extends Exception {
+    public CsvDataNotCorrectException(String message) {
+        super(message);
+    }
+
+    public CsvDataNotCorrectException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
