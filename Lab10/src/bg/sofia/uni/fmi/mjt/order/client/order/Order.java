d8af4e55ca2365173ecb696c7388ee012097ee00
Bayryam <116573641+Bayryam@users.noreply.github.com>
2024-01-13 18:54:54 +0200

    Delete Lab10 directory

--- Lab10/src/bg/sofia/uni/fmi/mjt/order/client/order/Order.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.order.client.order;
-
-import bg.sofia.uni.fmi.mjt.order.client.destination.Destination;
-import bg.sofia.uni.fmi.mjt.order.client.tshirt.TShirt;
-
-public record Order(int id, TShirt tShirt, Destination destination) {
-}
--- Lab10/src/bg/sofia/uni/fmi/mjt/order/client/tshirt/TShirt.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.order.client.tshirt;
-
-public record TShirt(Size size, Color color) {
-}
